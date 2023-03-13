518d39e036bc73aedf03a0bab76d1cf653ada232
Jessica Allman-LaPorte <90071482+JessA-L@users.noreply.github.com>
2023-03-13 15:44:41 -0400

    Refactor, fix microservice call bug

--- hockey_finder.py
+++ hockey_finder.py
@@ -3,8 +3,6 @@ import tkinter as tk
 import requests
 import microservice_connector
 
-bg_color = "#393e46"
-
 
 def gui() -> None:
     """
@@ -16,18 +14,20 @@ def gui() -> None:
     root.eval("tk::PlaceWindow . center")
 
     # create frame widgets
-    welcome_frame = tk.Frame(root, width=650, height=850, bg=bg_color)
-    results_frame = tk.Frame(root, bg=bg_color)
+    global welcome_frame
+    global results_frame
+    welcome_frame = tk.Frame(root, width=850, height=900)
+    results_frame = tk.Frame(root)
 
     for frame in (welcome_frame, results_frame):
         frame.grid(row=0, column=0)
 
-    load_welcome_frame(welcome_frame, results_frame)
+    load_welcome_frame()
 
     root.mainloop()
 
 
-def load_welcome_frame(welcome_frame, results_frame) -> None:
+def load_welcome_frame() -> None:
     """
     Creates and displays welcome frame
     """
@@ -35,10 +35,10 @@ def load_welcome_frame(welcome_frame, results_frame) -> None:
     welcome_frame.pack_propagate(False)
 
     # displays welcome frame widgets
-    welcome_frame_widgets(welcome_frame, results_frame)
+    welcome_frame_widgets()
 
 
-def title(welcome_frame):
+def title():
     """
     Create and display title
     Used by welcome_frame_widgets()
@@ -46,14 +46,13 @@ def title(welcome_frame):
     welcome_title = tk.Label(
         welcome_frame,
         text="Hockey Finder",
-        bg=bg_color,
         fg="red",
         font=("TkMenuFont", 28)
     )
     welcome_title.pack(pady=20)
 
 
-def description(welcome_frame):
+def description():
     """
     Create and display description
     Used by welcome_frame_widgets()
@@ -61,14 +60,12 @@ def description(welcome_frame):
     welcome_desc = tk.Label(
         welcome_frame,
         text="Find a hockey game near you!",
-        bg=bg_color,
-        fg="white",
         font=("TkMenuFont", 16)
     )
     welcome_desc.pack(pady=10)
 
 
-def instructions(welcome_frame):
+def instructions():
     """
     Create and display instructions
     Used by welcome_frame_widgets()
@@ -76,14 +73,12 @@ def instructions(welcome_frame):
     city_text = tk.Label(
         welcome_frame,
         text="Select either team or city.\n Then, input the name of the team or city you would like to search.",
-        bg=bg_color,
-        fg="white",
         font=("TkMenuFont", 12)
     )
     city_text.pack(pady=5)
 
 
-def radio_buttons(welcome_frame):
+def radio_buttons():
     """
     Create and display radio buttons
     Used by welcome_frame_widgets()
@@ -93,19 +88,21 @@ def radio_buttons(welcome_frame):
     radio_team = tk.Radiobutton(welcome_frame,
                                 text="Search by Team",
                                 variable=radio_sel,
-                                value="keyword"
+                                value="keyword",
+                                font=("TkMenuFont", 10)
                                 )
     radio_team.pack(pady=5)
 
     radio_city = tk.Radiobutton(welcome_frame,
                                 text="Search by City",
                                 variable=radio_sel,
-                                value="city"
+                                value="city",
+                                font=("TkMenuFont", 10)
                                 )
     radio_city.pack(pady=5)
 
 
-def input_box(welcome_frame) -> None:
+def input_box() -> None:
     """
     Create and display input box
     Used by welcome_frame_widgets()
@@ -117,7 +114,7 @@ def input_box(welcome_frame) -> None:
     input_box.pack(pady=5)
 
 
-def search_button(welcome_frame, results_frame) -> None:
+def search_button() -> None:
     """
     Create and display search button
     Used by welcome_frame_widgets()
@@ -126,32 +123,31 @@ def search_button(welcome_frame, results_frame) -> None:
                               text="Search",
                               font=("TkHeadingFont", 20),
                               bg="red",
-                              fg="white",
                               cursor="hand2",
                               activebackground="#BADEE2",
                               activeforeground="black",
-                              command=lambda: load_results(results_frame)
+                              command=lambda: load_results_frame()
                               )
     search_button.pack(pady=5)
 
 
-def welcome_frame_widgets(welcome_frame, results_frame) -> None:
+def welcome_frame_widgets() -> None:
     """
     Display widgets on welcome frame
     Used by load_welcome_frame()
     """
-    title(welcome_frame)
-    description(welcome_frame)
-    instructions(welcome_frame)
-    radio_buttons(welcome_frame)
-    input_box(welcome_frame)
-    search_button(welcome_frame, results_frame)
+    title()
+    description()
+    instructions()
+    radio_buttons()
+    input_box()
+    search_button()
 
 
 def get_radio_sel() -> str:
     """
     Gets input from radio selection
-    Used by load_results()
+    Used by load_results_frame()
     """
     radio_selected = radio_sel.get()
     return radio_selected
@@ -160,7 +156,7 @@ def get_radio_sel() -> str:
 def get_box_input() -> str:
     """
     Gets input from text box.
-    Used by load_results()
+    Used by load_results_frame()
     """
     box_input_var = tk.StringVar()
     box_input_var.set(input_box.get())
@@ -169,7 +165,7 @@ def get_box_input() -> str:
     return box_input
 
 
-def load_results(results_frame) -> None:
+def load_results_frame() -> None:
     """
     Creates results frame, calls ticketmaster API, displays cheapest and soonest games in GUI
     Used by search_button()
@@ -177,16 +173,30 @@ def load_results(results_frame) -> None:
     results_frame.tkraise()
     call_ticketmaster(get_radio_sel(), get_box_input())
 
+    load_results()
+
+
+def load_results():
+    """
+    Display results from microservice call
+    Used by load_results_frame
+    """
     soonest, cheapest = get_cheapest_and_soonest()
+
+    if soonest:
+        soonest_text = f'Soonest Game: {soonest[0]["name"]} on {soonest[0]["dates"]["start"]["localDate"]}\n'
+    else:
+        soonest_text = "Error: cannot find soonest game"
+    if cheapest:
+        cheapest_text = f'Cheapest Game: {cheapest[0]["name"]} at {cheapest[0]["priceRanges"][0]["min"]}\n'
+    else:
+        cheapest_text = "Error: cannot find cheapest game"
+
     tk.Label(
         results_frame,
-        text=f'Soonest Game: {soonest[0]["name"]} on {soonest[0]["dates"]["start"]["localDate"]}\n'
-             f'Cheapest Game: {cheapest[0]["name"]} at {cheapest[0]["priceRanges"][0]["min"]}',
-        bg=bg_color,
-        fg="white",
+        text=f'{soonest_text}\n{cheapest_text}',
         font=("TkMenuFont", 12)
     ).pack()
-
 
 def get_cheapest_and_soonest() -> json:
     """
@@ -204,7 +214,7 @@ def get_cheapest_and_soonest() -> json:
 def call_ticketmaster(radio_choice, search_input):
     """
     Calls ticketmaster API using user provided search parameters and writes results into a JSON file.
-    Used by load_results()
+    Used by load_results_frame()
     """
     endpoint = "https://app.ticketmaster.com/discovery/v2/events.json"
     api_key = "XSsjkkAUauyyZ0nRH5AxLkWXNs3JTLNY"
