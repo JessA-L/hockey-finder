306a75189812655cd12c49b809b61870b797c32d
Jessica Allman-LaPorte <90071482+JessA-L@users.noreply.github.com>
2023-03-13 13:17:58 -0400

    Refactor and change colors

--- hockey_finder.py
+++ hockey_finder.py
@@ -5,17 +5,18 @@ import microservice_connector
 
 bg_color = "#393e46"
 
+
 def gui() -> None:
     """
     Generates gui.
     """
-    # initiallize app
+    # initialize app
     root = tk.Tk()
     root.title("Hockey Finder")
     root.eval("tk::PlaceWindow . center")
 
-    # create a frame widget
-    welcome_frame = tk.Frame(root, width=650, height=650, bg=bg_color)
+    # create frame widgets
+    welcome_frame = tk.Frame(root, width=650, height=850, bg=bg_color)
     results_frame = tk.Frame(root, bg=bg_color)
 
     for frame in (welcome_frame, results_frame):
@@ -23,25 +24,27 @@ def gui() -> None:
 
     load_welcome_frame(welcome_frame, results_frame)
 
-    # Create loop
     root.mainloop()
+
 
 def load_welcome_frame(welcome_frame, results_frame) -> None:
     welcome_frame.tkraise()
     welcome_frame.pack_propagate(False)
 
-    # create welcome frame widgets
+    # creates and displays welcome frame widgets
     welcome_frame_widgets(welcome_frame, results_frame)
+
 
 def display_welcome_title(welcome_frame):
     welcome_title = tk.Label(
         welcome_frame,
         text="Hockey Finder",
         bg=bg_color,
-        fg="white",
-        font=("TkMenuFont", 26)
+        fg="red",
+        font=("TkMenuFont", 28)
     )
     welcome_title.pack(pady=20)
+
 
 def display_welcome_desc(welcome_frame):
     welcome_desc = tk.Label(
@@ -49,30 +52,29 @@ def display_welcome_desc(welcome_frame):
         text="Find a hockey game near you!",
         bg=bg_color,
         fg="white",
-        font=("TkMenuFont", 14)
+        font=("TkMenuFont", 16)
     )
     welcome_desc.pack(pady=10)
 
-def display_city_text(welcome_frame):
 
+def display_city_text(welcome_frame):
     city_text = tk.Label(
         welcome_frame,
-        text="Input city: ",
+        text="Select either team or city.\n Then, input the name of the team or city you would like to search.",
         bg=bg_color,
         fg="white",
-        font=("TkMenuFont", 14)
+        font=("TkMenuFont", 12)
     )
     city_text.pack(pady=5)
 
-def display_radios(welcome_frame):
 
+def display_radios(welcome_frame):
     global radio_sel
     radio_sel = tk.StringVar()
     radio_team = tk.Radiobutton(welcome_frame,
                                 text="Search by Team",
                                 variable=radio_sel,
                                 value="keyword"
-                                # command=lambda: get_radio_sel()
                                 )
     radio_team.pack(pady=5)
 
@@ -80,18 +82,16 @@ def display_radios(welcome_frame):
                                 text="Search by City",
                                 variable=radio_sel,
                                 value="city"
-                                # command=lambda: get_radio_sel()
                                 )
     radio_city.pack(pady=5)
 
-def get_radio_sel():
+
+def get_radio_sel() -> str:
     radio_selected = radio_sel.get()
-    print(radio_selected)
     return radio_selected
 
 
-def display_input_box(welcome_frame):
-
+def display_input_box(welcome_frame) -> None:
     global input_box
     input_box = tk.Entry(welcome_frame,
                          width=10
@@ -99,12 +99,11 @@ def display_input_box(welcome_frame):
     input_box.pack(pady=5)
 
 
-def display_search_button(welcome_frame, results_frame):
-
+def display_search_button(welcome_frame, results_frame) -> None:
     search_button = tk.Button(welcome_frame,
                               text="Search",
                               font=("TkHeadingFont", 20),
-                              bg="darkred",
+                              bg="red",
                               fg="white",
                               cursor="hand2",
                               activebackground="#BADEE2",
@@ -113,8 +112,9 @@ def display_search_button(welcome_frame, results_frame):
                               )
     search_button.pack(pady=5)
 
-def welcome_frame_widgets(welcome_frame, results_frame):
 
+def welcome_frame_widgets(welcome_frame, results_frame) -> None:
+    """"""
     display_welcome_title(welcome_frame)
     display_welcome_desc(welcome_frame)
     display_city_text(welcome_frame)
@@ -122,22 +122,19 @@ def welcome_frame_widgets(welcome_frame, results_frame):
     display_input_box(welcome_frame)
     display_search_button(welcome_frame, results_frame)
 
-def get_box_input():
+
+def get_box_input() -> str:
     box_input_var = tk.StringVar()
     box_input_var.set(input_box.get())
     box_input = box_input_var.get()
-    print("Box input:", box_input)
 
     return box_input
 
-def load_city_results(results_frame):
 
+def load_city_results(results_frame) -> None:
     results_frame.tkraise()
 
     call_ticketmaster(get_radio_sel(), get_box_input())
-                
-    # Print results in CLI
-    output_results_in_CLI()
 
     # Display soonest game in ui
     soonest, cheapest = get_cheapest_and_soonest()
@@ -149,57 +146,41 @@ def load_city_results(results_frame):
         fg="white",
         font=("TkMenuFont", 12)
     ).pack()
-    
-def output_results_in_CLI() -> None:
-    """
-    Prints results in terminal.
-    """
-    with open('tm-results.json', 'r') as infile:
-        test_data = json.load(infile)
-    for i in range(9):
-        print(test_data["_embedded"]["events"][i]["name"] + " " + test_data["_embedded"]["events"][i]["dates"]["start"]["localDate"])
-        
+
+
 def get_cheapest_and_soonest():
     """
-    Connects to hockey microservice using zeroMQ, sends a request containing hockey games, and returns the soonest and cheapest games.
+    Connects to hockey microservice using zeroMQ, sends a request containing hockey games,
+    and returns the soonest and cheapest games.
     """
     socket = microservice_connector.connect_to_microservice()
     reply = microservice_connector.microservice_call(socket)
     soonest, cheapest = reply[0], reply[1]
-    
-    # Example print for the games returned
-    print()
-    print('Soonest games:')
-    for event in soonest:
-        print(f'{event["name"]} at {event["dates"]["start"]["localDate"]}')
-    if cheapest:
-        print('Cheapest games:')
-        for event in cheapest:
-            print(f'{event["name"]} at {event["priceRanges"][0]["min"]}')
-    
+
     return soonest, cheapest
 
-def call_ticketmaster(radio_sel, search_input):
-    # Set the API ENDPOINT and API key
-    ENDPOINT = "https://app.ticketmaster.com/discovery/v2/events.json"
-    API_KEY = "XSsjkkAUauyyZ0nRH5AxLkWXNs3JTLNY"
 
-    # Set the search parameters
+def call_ticketmaster(radio_choice, search_input):
+    """
+    Calls ticketmaster API using user provided search parameters and writes results into a JSON file.
+    """
+    endpoint = "https://app.ticketmaster.com/discovery/v2/events.json"
+    api_key = "XSsjkkAUauyyZ0nRH5AxLkWXNs3JTLNY"
+
     params = {
-        "apikey": API_KEY,
+        "apikey": api_key,
         "keyword": "hockey",
-        radio_sel: search_input,
+        radio_choice: search_input,
         "radius": 100,
         "size": 10,
     }
 
-    # Make the API call
-    response = requests.get(ENDPOINT, params=params)
+    response = requests.get(endpoint, params=params)
 
-    # Output the response to a JSON file
     event_list = response.json()
     with open('tm-results.json', 'w') as outfile:
         json.dump(event_list, outfile)
+
 
 if __name__ == "__main__":
     gui()
